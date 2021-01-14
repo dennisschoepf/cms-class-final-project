52b333d4ada238098bbd5d53f26702acf362c6b9
Dennis Schoepf <me@dnsc.io>
2021-01-14 14:25:56 +0100

    Implement links and other things on front page

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -3,43 +3,43 @@
 /* Custom Code */
 function getHashedStyleSheet()
 {
-  $dirCSS = new DirectoryIterator(get_stylesheet_directory());
-  foreach ($dirCSS as $file) {
-    if (pathinfo($file, PATHINFO_EXTENSION) === 'css') {
-      $fullName = basename($file);
-      if (strpos($fullName, 'admin') !== false) {
-        return $fullName;
-      }
-    }
-  }
+	$dirCSS = new DirectoryIterator(get_stylesheet_directory());
+	foreach ($dirCSS as $file) {
+		if (pathinfo($file, PATHINFO_EXTENSION) === 'css') {
+			$fullName = basename($file);
+			if (strpos($fullName, 'admin') !== false) {
+				return $fullName;
+			}
+		}
+	}
 }
 
 function add_theme_scripts()
 {
-  wp_enqueue_style('style', get_stylesheet_uri());
-  wp_enqueue_style('custom-style', get_template_directory_uri() . '/' . getHashedStyleSheet(), array('style'));
-  wp_enqueue_script('main-script', get_template_directory_uri() . '/main.js');
-  wp_enqueue_script('admin-script', get_template_directory_uri() . '/admin.js');
+	wp_enqueue_style('style', get_stylesheet_uri());
+	wp_enqueue_style('custom-style', get_template_directory_uri() . '/' . getHashedStyleSheet(), array('style'));
+	wp_enqueue_script('main-script', get_template_directory_uri() . '/main.js');
+	wp_enqueue_script('admin-script', get_template_directory_uri() . '/admin.js');
 }
 
-/*function register_my_menus()
+function register_my_menus()
 {
-  register_nav_menus(
-    array(
-      'header-menu' => __('Header Menu'),
-      'footer-menu' => __('Footer Menu')
-    )
-  );
-}*/
+	register_nav_menus(
+		array(
+			'header-menu' => __('Header Menu'),
+			'footer-menu' => __('Footer Menu')
+		)
+	);
+}
 
 function cc_mime_types($mimes)
 {
-  $mimes['svg'] = 'image/svg+xml';
-  return $mimes;
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
 }
 
 add_filter('upload_mimes', 'cc_mime_types');
-// add_action('init', 'register_my_menus');
+add_action('init', 'register_my_menus');
 add_action('wp_enqueue_scripts', 'add_theme_scripts');
 add_theme_support('post-thumbnails');
 // add_filter('acf/settings/remove_wp_meta_box', '__return_false');
@@ -61,7 +61,7 @@ add_theme_support('post-thumbnails');
  * plug-in, you can safely delete this block.
  */
 $composer_autoload = __DIR__ . '/vendor/autoload.php';
-if ( file_exists( $composer_autoload ) ) {
+if (file_exists($composer_autoload)) {
 	require_once $composer_autoload;
 	$timber = new Timber\Timber();
 }
@@ -70,18 +70,18 @@ if ( file_exists( $composer_autoload ) ) {
  * This ensures that Timber is loaded and available as a PHP class.
  * If not, it gives an error message to help direct developers on where to activate
  */
-if ( ! class_exists( 'Timber' ) ) {
+if (!class_exists('Timber')) {
 
 	add_action(
 		'admin_notices',
-		function() {
-			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+		function () {
+			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
 		}
 	);
 
 	add_filter(
 		'template_include',
-		function( $template ) {
+		function ($template) {
 			return get_stylesheet_directory() . '/static/no-timber.html';
 		}
 	);
@@ -91,7 +91,7 @@ if ( ! class_exists( 'Timber' ) ) {
 /**
  * Sets the directories (inside your theme) to find .twig files
  */
-Timber::$dirname = array( 'templates', 'views' );
+Timber::$dirname = array('templates', 'views');
 
 /**
  * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
@@ -104,30 +104,33 @@ Timber::$autoescape = false;
  * We're going to configure our theme inside of a subclass of Timber\Site
  * You can move this to its own file and include here via php's include("MySite.php")
  */
-class StarterSite extends Timber\Site {
+class StarterSite extends Timber\Site
+{
 	/** Add timber support. */
-	public function __construct() {
-		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
-		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
-		add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
-		add_action( 'init', array( $this, 'register_post_types' ) );
-		add_action( 'init', array( $this, 'register_taxonomies' ) );
+	public function __construct()
+	{
+		add_action('after_setup_theme', array($this, 'theme_supports'));
+		add_filter('timber/context', array($this, 'add_to_context'));
+		add_filter('timber/twig', array($this, 'add_to_twig'));
+		add_action('init', array($this, 'register_post_types'));
+		add_action('init', array($this, 'register_taxonomies'));
 		parent::__construct();
 	}
 	/** This is where you can register custom post types. */
-	public function register_post_types() {
-
+	public function register_post_types()
+	{
 	}
 	/** This is where you can register custom taxonomies. */
-	public function register_taxonomies() {
-
+	public function register_taxonomies()
+	{
 	}
 
 	/** This is where you add some context
 	 *
 	 * @param string $context context['this'] Being the Twig's {{ this }}.
 	 */
-	public function add_to_context( $context ) {
+	public function add_to_context($context)
+	{
 		$context['foo']   = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
@@ -136,9 +139,10 @@ class StarterSite extends Timber\Site {
 		return $context;
 	}
 
-	public function theme_supports() {
+	public function theme_supports()
+	{
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
 		 * Let WordPress manage the document title.
@@ -146,14 +150,14 @@ class StarterSite extends Timber\Site {
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
 
 		/*
 		 * Switch default core markup for search form, comment form, and comments
@@ -187,14 +191,15 @@ class StarterSite extends Timber\Site {
 			)
 		);
 
-		add_theme_support( 'menus' );
+		add_theme_support('menus');
 	}
 
 	/** This Would return 'foo bar!'.
 	 *
 	 * @param string $text being 'foo', then returned 'foo bar!'.
 	 */
-	public function myfoo( $text ) {
+	public function myfoo($text)
+	{
 		$text .= ' bar!';
 		return $text;
 	}
@@ -203,12 +208,12 @@ class StarterSite extends Timber\Site {
 	 *
 	 * @param string $twig get extension.
 	 */
-	public function add_to_twig( $twig ) {
-		$twig->addExtension( new Twig\Extension\StringLoaderExtension() );
-		$twig->addFilter( new Twig\TwigFilter( 'myfoo', array( $this, 'myfoo' ) ) );
+	public function add_to_twig($twig)
+	{
+		$twig->addExtension(new Twig\Extension\StringLoaderExtension());
+		$twig->addFilter(new Twig\TwigFilter('myfoo', array($this, 'myfoo')));
 		return $twig;
 	}
-
 }
 
 new StarterSite();
