5d717bfe404546c63ffe8628137f360b2c6883e8
Dennis Schoepf <me@dnsc.io>
2021-01-15 13:40:34 +0100

    Finish up stories and inventions page

--- src/theme/page.php
+++ src/theme/page.php
@@ -1,7 +1,23 @@
 <?php
 $context = Timber::context();
 
+$stories_args = array(
+  'post_type' => 'post',
+  'category_name' => 'stories',
+  'numberposts' => 3,
+  'order' => 'DESC'
+);
+
+$inventions_args = array(
+  'post_type' => 'post',
+  'category_name' => 'inventions',
+  'numberposts' => 3,
+  'order' => 'DESC'
+);
+
 $context['post'] = Timber::get_post();
+$context['inventions'] = Timber::get_posts($inventions_args);
+$context['stories'] = Timber::get_posts($stories_args);
 
 /*echo "<pre>";
 print_r($context['post']->slug);
